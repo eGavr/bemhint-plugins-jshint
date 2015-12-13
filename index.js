4d5c02fa478016fb6b6a4496af0c08c5f809fb3b
Evgeniy Gavryushin <job.egavr@yandex.ru>
2015-12-13 15:59:45 +0300

    add a removing of points at the end of error messages

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ exports.forEntityTech = function(tech, techConfig, entity) {
 
     jshint.data().errors && jshint.data().errors.forEach(function(error) {
         entity.addError({
-            msg: error.reason,
+            msg: error.reason.replace(/\.$/, ''),
             tech: tech.name,
             value: 'line ' + error.line + ', character ' + error.character
         });
