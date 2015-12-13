270a9c6d5083c56749e77bb2b2d196d693f8829b
Evgeniy Gavryushin <job.egavr@yandex.ru>
2015-12-13 15:07:28 +0300

    Ok! Let's go!

---
+++ index.js
@@ -0,0 +1,14 @@
+var jshint = require('jshint').JSHINT;
+
+///
+exports.forEntityTech = function(tech, techConfig, entity) {
+    jshint(tech.content, techConfig);
+
+    jshint.data().errors && jshint.data().errors.forEach(function(error) {
+        entity.addError({
+            msg: error.reason,
+            tech: tech.name,
+            value: 'line ' + error.line + ', character ' + error.character
+        });
+    });
+};
